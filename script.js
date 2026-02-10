5bbb4e33532876ebeffb27ab7ee6097a7093ba60
Vishwam002 <vishwamsrivastava.1234@gmail.com>
2026-02-11 02:19:12 +0530

    script.js

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ form.addEventListener('submit', (e) => {
 		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
-		resultDiv.innerHTML = 'Naa Tum meri baby nhi hoo kon hooo tum kaha hai meri Madhunika';
+		resultDiv.innerHTML = 'Hmph! Not allowed,this was made for her💖';
 	}
 });
